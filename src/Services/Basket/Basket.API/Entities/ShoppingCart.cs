c775e8d97745e84d2c13efe24547b7afb5bc2482
Jency Raj <jencyraj91@gmail.com>
2021-09-18 15:25:08 +0200

    Initial

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+        public ShoppingCart()
+        {
+
+        }
+        public ShoppingCart(String userName)
+        {
+            UserName = userName;
+        }
+        //need to calculate the tottal price  
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalprice = 0;
+                foreach(var item in Items)
+                {
+                    totalprice += item.Price * item.Quantity;
+
+                }
+                return totalprice;
+            }
+
+        }
+
+    }
+}
